22c00b804fdb7e4b6fa4b4da2354d09be289b47b
dvitorio <dvitorio2@gmail.com>
2021-11-03 16:53:53 -0300

    Construção da tela de relação cintura/quadril

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+//Cálculo do IMC
 function calcularIMC(){
-    var peso = parseFloat(document.getElementById("peso").value);
-    var altura = parseFloat(document.getElementById("altura").value);
-    var imc = peso/(altura * altura);
+    let peso = parseFloat(document.getElementById("peso").value);
+    let altura = parseFloat(document.getElementById("altura").value);
+    let imc = peso/(altura * altura);
         if(imc > 0 && imc < 18.5){
             let texto = document.getElementById("resultado");
             texto.innerText = "Você está abaixo do peso!";
@@ -34,7 +35,6 @@ function calcularIMC(){
         }
     document.getElementById("imc").value = imc.toFixed(2);   
 }
-
 function limpar(){
     if(peso!=0 && altura!=0 && imc!=0 && resultado!=0){
         document.getElementById("peso").value = " ";
@@ -43,6 +43,23 @@ function limpar(){
         document.getElementById("resultado").value = " ";
         var texto = document.getElementById("resultado");
         texto.innerText = " ";
-    }
-    
+    }   
 }
+//Fim do cálculo de IMC
+
+
+//Cálculo da relação cintura/quadril
+
+function cinturaQuadril(){
+    let cintura = parseFloat(document.getElementById("cintura").value);
+    let quadril = parseFloat(document.getElementById("quadril").value);
+        if((cintura >= 30 && cintura <= 250) && (quadril >= 50 && quadril <=350)){
+            let rcq = (cintura/quadril);
+            document.getElementById("rcq").value = rcq.toFixed(2);
+        }
+        else{
+            alert("Valor(es) da cintura ou quadril inválido(s)! Confira os dados e digite novamente!");
+        }
+
+}   
+

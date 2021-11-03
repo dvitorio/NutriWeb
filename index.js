2f021fb2fa8b37ba1f555a9d9f778bb717d8324e
dvitorio <dvitorio2@gmail.com>
2021-11-03 00:54:14 -0300

    Integração de tela de cálculo de imc no index.html

---
+++ index.js
@@ -0,0 +1,36 @@
+function calcularIMC(){
+    var peso = parseFloat(document.getElementById("peso").value);
+    var altura = parseFloat(document.getElementById("altura").value);
+    var imc = peso/(altura * altura);
+        if(imc > 0 && imc < 18.5){
+            let texto = document.getElementById("resultado");
+            texto.innerText = "Você está abaixo do peso!";
+            texto.style.color = 'white';
+        }
+        else if(imc >= 18.5 && imc <= 24.9){
+            let texto = document.getElementById("resultado");
+            texto.innerText = "Você está com peso normal. Parabéns!";
+            texto.style.color = 'white';
+        }
+        else if(imc >= 25 && imc <= 29.9){
+            let texto = document.getElementById("resultado");
+            texto.innerText = "Cuidado! Você está com sobrepeso!";
+            texto.style.color = 'orangered';
+        }
+        else if(imc >= 30 && imc <= 34.9){
+            let texto = document.getElementById("resultado");
+            texto.innerText = "Perigo! Você está com obesidade Grau I! Procure um nutricionista ou médico";
+            texto.style.color = 'red';
+        }
+        else if(imc >= 35 && imc <= 39.9){
+            let texto = document.getElementById("resultado");
+            texto.innerText = "Perigo! Você está com obesidade Grau II! Procure um nutricionista ou médico";
+            texto.style.color = 'red';
+        }
+        else if(imc >= 40){
+            let texto = document.getElementById("resultado");
+            texto.innerText = "Perigo! Você está com obesidade mórbida! Procure um nutricionista ou médico";
+            texto.style.color = 'red';
+        }
+    document.getElementById("imc").value = imc.toFixed(2);   
+}
